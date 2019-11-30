c723f57f9d670961796e8453acce86eacaa4a39f
Gio3118 <43028302+Gio3118@users.noreply.github.com>
2019-11-30 23:33:17 +0100

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -352,15 +352,6 @@ class BST:
         else:
             return "Lege boom"
 
-    def inorderTraversal(self):
-        """
-       :method: Geeft de elementen van de boom van klein naar groot weer.
-       :input: Geen.
-       :output: Geeft de elementen weer van klein naar groot, geeft "True" als succes en "False" voor mislukking.
-       :pre-condition: Een boom met elementen.
-       :post-condition: Geen.
-
-        """
     def isEmpty(self):
         """
        :method: Toont aan of de BST leeg is of niet.
@@ -419,3 +410,96 @@ class BST:
         while successor.leftTree is not None:
             successor = successor.leftTree
         return successor
+    
+#--------------------------- onderstaande code om BST te testen ---------------------------
+    
+
+def printTree(tree, curr_index, index=False, delimiter='-'):
+
+    if tree == None or tree.root is None:
+        return [], 0, 0, 0
+
+    line1 = []
+    line2 = []
+    if index:
+        node_repr = '{}{}{}'.format(curr_index, delimiter, tree.root.match)
+    else:
+        node_repr = str(tree.root.match)
+
+    new_root_width = gap_size = len(node_repr)
+
+    # Get the left and right sub-boxes, their widths, and root repr positions
+    l_box, l_box_width, l_root_start, l_root_end = \
+        printTree(tree.leftTree, 2 * curr_index + 1, index, delimiter)
+    r_box, r_box_width, r_root_start, r_root_end = \
+        printTree(tree.rightTree, 2 * curr_index + 2, index, delimiter)
+
+    # Draw the branch connecting the current root node to the left sub-box
+    # Pad the line with whitespaces where necessary
+    if l_box_width > 0:
+        l_root = (l_root_start + l_root_end) // 2 + 1
+        line1.append(' ' * (l_root + 1))
+        line1.append('_' * (l_box_width - l_root))
+        line2.append(' ' * l_root + '/')
+        line2.append(' ' * (l_box_width - l_root))
+        new_root_start = l_box_width + 1
+        gap_size += 1
+    else:
+        new_root_start = 0
+
+    # Draw the representation of the current root node
+    line1.append(node_repr)
+    line2.append(' ' * new_root_width)
+
+    # Draw the branch connecting the current root node to the right sub-box
+    # Pad the line with whitespaces where necessary
+    if r_box_width > 0:
+        r_root = (r_root_start + r_root_end) // 2
+        line1.append('_' * r_root)
+        line1.append(' ' * (r_box_width - r_root + 1))
+        line2.append(' ' * r_root + '\\')
+        line2.append(' ' * (r_box_width - r_root))
+        gap_size += 1
+    new_root_end = new_root_start + new_root_width - 1
+
+    # Combine the left and right sub-boxes with the branches drawn above
+    gap = ' ' * gap_size
+    new_box = [''.join(line1), ''.join(line2)]
+    for i in range(max(len(l_box), len(r_box))):
+        l_line = l_box[i] if i < len(l_box) else ' ' * l_box_width
+        r_line = r_box[i] if i < len(r_box) else ' ' * r_box_width
+        new_box.append(l_line + gap + r_line)
+
+    # Return the new box, its width and its root repr positions
+    return new_box, len(new_box[0]), new_root_start, new_root_end
+
+
+BST = BST()
+
+BST.insert(Node(0.8))  
+BST.insert(Node(0.5))
+BST.insert(Node(0.2))
+BST.insert(Node(0.3))
+BST.insert(Node(0.99))
+BST.insert(Node(1))
+
+BST.insert(Node(0.1))
+
+print(BST)
+BST.delete(0.8)
+print(BST)
+BST.delete(0.99)
+BST.delete(1)
+BST.delete(0.2)
+BST.delete(0.1)
+BST.delete(0.5)
+print(BST)
+BST.delete(0.3)
+print(BST)
+BST.insert(Node(0.5))
+BST.insert(Node(0.8))
+BST.insert(Node(0.3))
+BST.insert(Node(0.4))
+print(BST)
+BST.delete(0.3)
+print(BST)
